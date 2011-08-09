8a5204ca7f8f6506e1f793fa07f456d73f90a380
Kalid Azad <kalid.azad@gmail.com>
2011-08-08 23:19:56 -0700

    Revert 62a0a35cbe212838c23fbc37491d90b9e5df3b4a^..HEAD

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
         <link rel="shortcut icon" href="/favicon.ico" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript />
 
 	<?php wp_head(); ?>
 
--- single.php
+++ single.php
@@ -26,10 +26,6 @@ foreach($posts as $post) { start_wp();
 	<hr class="hr1" />
 <?php } ?>
 
-	<!--
-	<?php trackback_rdf(); ?>
-	-->
-
 <script src="http://feeds.feedburner.com/~s/Betterexplained?i=<?php the_permalink() ?>" type="text/javascript" charset="utf-8"></script>
 
 <?php 
@@ -50,18 +46,61 @@ foreach($posts as $post) { start_wp();
 
 <hr class="hr1" />
 
+<div class="meta" style="float:right;">
+	<?php the_time("F j, Y") ?>
+</div>
+
 <iframe src="http://www.facebook.com/plugins/like.php?href=betterexplained.com&amp;layout=button_count&amp;show_faces=false&amp;width=100&amp;action=like&amp;font&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
 
-	<div class="meta">Posted <?php the_time("F j, Y") ?>, 
-<?php _e(" under "); ?> <?php the_category(',') ?><br>
-<?php _e("Tags:"); ?> 
-<?php if (function_exists('UTW_ShowTagsForCurrentPost')) UTW_ShowTagsForCurrentPost("commalist"); ?>
+<div class="meta">
+
+<?php 
+$categories = get_the_category();
+$category_data = array();
+$current_id = $post->ID;
+
+$related_posts = array();
+
+foreach($categories as $category) {
+	$category_data[$category->cat_ID] = array();
+	
+	$category_posts = new WP_Query("cat=" . $category->cat_ID . "&showposts=6");
+	while($category_posts->have_posts()) {
+		$category_posts->the_post();
+				
+		if ($post->ID != $current_id) {
+			$data = array('permalink' => get_permalink($post->ID), 'title' => $post->post_title);
+			$category_data[$category->cat_ID][] = $data;
+		}
+		
+		if (!is_null($category->parent)) {
+			$default_category_id = $category->parent;
+		}
+	}
+	
+	wp_reset_postdata();
+}
+
+?>
+
+Other Posts in <?php the_category(',') ?>
+<?php foreach($category_data as $items) : ?>
+<ul class="related-posts">
+	<?php $count = 0; ?>
+	<?php foreach($items as $data) : ?>
+		<li><a href="<?php echo $data['permalink']; ?>"><?php echo $data['title']; ?></a></li>
+		<?php $count++; if ($count >= 3) { break; } ?>
+	<?php endforeach; ?>
+</ul>
+<?php endforeach; ?>
+
+<?php if (has_tag()) : ?>
 <br/>
-Related Posts: 
+<?php _e("Tags:"); ?>
+<?php if (function_exists('UTW_ShowTagsForCurrentPost')) UTW_ShowTagsForCurrentPost("commalist"); ?>
+<?php endif; ?>
 
-<?php if (function_exists('related_posts')) related_posts(5, 10, '< li >', '< /li >',"","",false, false); ?>
-
-<?php if (function_exists('sociable_html')) print sociable_html(); ?>
+<br/>
 
 <div>
 <a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS'); ?>">
@@ -79,9 +118,7 @@ Related Posts:
 
 <style>
 .adbox{
-  xborder: 1px solid #ccc;
   padding: 5px;
-  xfont-size: 1.2em;
   background: #f1f1f1;
   border: 2px solid #eaeaea;
 }
@@ -98,7 +135,7 @@ Related Posts:
 </div>
 <div style="float: left; width: 340px; height: 100%; padding-left: 20px;">
 <h2>BetterExplained: Get the eBook</h2>
-Like the site? A lot? Take it with you. Develop your math sense using insights, not memorization.
+Like the site? Take it with you. Develop your math sense using insights, not memorization.
 
 <div style="padding-top: 10px;">
 <a href="http://betterexplained.com/articles/math-betterexplained-ebook-available/">
@@ -107,10 +144,43 @@ Like the site? A lot? Take it with you. Develop your math sense using insights, 
 </div>
 </div>
 <div style="clear: both;"></div>
-<?php if (function_exists('bex_premium')) : ?>
-	<?php echo bex_premium(); ?>
-<?php endif; ?>
 </div>
+
+<!-- 
+<hr class="hr1" />
+
+<div>
+<div class="meta">Tools of the trade:</div>
+<div class="amz-items">
+<div class="amz-item">
+<a href="http://www.amazon.com/exec/obidos/ASIN/0789723107/bettere-20"><img src="http://images.amazon.com/images/P/0789723107.01.THUMBZZZ.jpg" alt="Book Cover" /><br/>Don't Make Me Think <br/>(Web Design)</a>
+</div>
+
+<div class="amz-item">
+<a href="http://www.amazon.com/exec/obidos/ASIN/0060891548/bettere-20"><img src="http://images.amazon.com/images/P/0060891548.01.THUMBZZZ.jpg" alt="Book Cover" /><br/>On Writing Well <br/>(Writing Style)</a>
+</div>
+
+<div class="amz-item">
+<a href="http://www.amazon.com/exec/obidos/ASIN/B000HCZ8EO/bettere-20"><img src="http://images.amazon.com/images/P/B000HCZ8EO.01.THUMBZZZ.jpg" alt="Book Cover" /><br/>MS Office 2007 <br/>(Charts & Diagrams)</a>
+</div>
+<div class="clear"/>
+</div>
+<br/>
+-->
+
+<script type="text/javascript"><!--
+google_ad_client = "pub-0990604499205651";
+//BetterExplained Bottom, 468x60
+google_ad_slot = "1313029194";
+google_ad_width = 468;
+google_ad_height = 60;
+//--></script>
+
+<!--
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+-->
 
 <hr class="hr1" />
 
